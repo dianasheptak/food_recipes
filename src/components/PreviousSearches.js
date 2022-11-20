27f86d9858eccf0c01cd24138934445ea9a7ae9c
dianasheptak <a234@234s-MacBook-Pro.local>
2022-11-20 23:22:24 +0200

    food recipes

---
+++ src/components/PreviousSearches.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
+
+
+export default function PreviousSearches(){
+    const searches = ['pizza', 'burger', 'cookies', 'juice', 'biriyani', 'salad', 'ice cream', 'lasagna', 'pudding', 'soup'] 
+
+    return (
+        <div className="previous-searches section">
+            <h2>Previous Searches</h2>
+            <div className="previous-searches-container">
+                {
+                    searches.map((search, idx) => (
+                        <div className="search-item" key={idx} style={{animationDelay: idx * .08 + 's'}}>
+                            {search}
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="search-box">
+                <input type="text" placeholder="Search ..." />
+                <button className="btn">
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
+            </div> 
+        </div>
+    )
+}
